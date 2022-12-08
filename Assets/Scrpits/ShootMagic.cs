c229a1e84ef2d5033b2d28281d19697cc813b913
baconman465 <ps24acolburn@efcts.us>
2022-12-08 17:38:21 -0500

    fixed shooting REAL

--- Assets/Scrpits/ShootMagic.cs
+++ Assets/Scrpits/ShootMagic.cs
@@ -23,15 +23,18 @@ public class ShootMagic : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+             if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                magicBolt.transform.rotation = new Quaternion(0, 0, 180, 0);
+            }
+            else
+            {
+                magicBolt.transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
             projectileClass bolt = new projectileClass();
             Instantiate<GameObject>(magicBolt, firePoint);
             magicBolt.transform.SetParent(null);
-            magicBolt.transform.rotation = new Quaternion(0, 0, 0, 0);
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                
-                magicBolt.transform.rotation = new Quaternion(0, 0, 180, 0);
-            }
+           
         }
             
     }
